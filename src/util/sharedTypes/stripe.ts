2dccbf78016b9c00b521fe160502ee034b8c0665
Louis Sankey <lmsankey@ucdavis.edu>
2023-03-27 10:16:41 -0700

    replaced the shared symlink with a regular directory for deployment purposes

---
+++ src/util/sharedTypes/stripe.ts
@@ -0,0 +1,8 @@
+import Stripe from 'stripe'
+
+const stripe: any = new Stripe(process.env.STRIPE_KEY!, {
+  apiVersion: '2022-11-15',
+  typescript: true
+})
+
+export default stripe
---
+++ src/util/sharedTypes/types.ts
@@ -0,0 +1,19 @@
+export interface Session {
+  id?: string
+  sessionToken?: string
+  accessToken?: string
+  userId?: string
+  expires?: string
+  user?: User
+}
+
+export interface User {
+  id?: string
+  username?: string
+  email: string
+  emailVerified?: boolean
+  image?: string
+  name?: string
+  tier?: string
+  stripeCustomerId?: string
+}
