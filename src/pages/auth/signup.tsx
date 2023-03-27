77b82b9da273427635e2c4a1875727f76e376206
Louis Sankey <lmsankey@ucdavis.edu>
2023-03-27 10:12:30 -0700

    replaced the shared symlink with a regular directory for deployment purposes

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import LandingTopNav from '../../components/Nav/LandingTopNav'
 
 import { useRouter } from 'next/router'
-import SharedSignUp from '../../shared/screens/signup'
+import SharedSignUp from '../../shared/screens/signUp'
 
 type ValidatorOptionType = number | boolean
 
