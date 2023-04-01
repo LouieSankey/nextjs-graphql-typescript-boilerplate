5783d8e0715fd2a6145fcf106306698aa3304164
Louis Sankey <lmsankey@ucdavis.edu>
2023-04-01 11:15:53 -0700

    temporarily made stripe key public for testing

--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -1,5 +1,5 @@
 import Stripe from 'stripe' //hello world 5000
-const stripe: any = new Stripe(process.env.NEXT_PUBLIC_STRIPE_KEY!, {
+const stripe: any = new Stripe(process.env.STRIPE_KEY!, {
   apiVersion: '2022-11-15',
   typescript: true
 })
