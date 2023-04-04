cc7f125883e645c80f58065e3891e83bb5072721
Louis Sankey <lmsankey@ucdavis.edu>
2023-04-03 18:20:11 -0700

    completely renamed the webhook and readded on stripe, and now it seems to be working

--- src/pages/api/webhooks/stripe-webhook.ts
+++ src/pages/api/webhooks/stripe-webhook.ts
@@ -4,7 +4,6 @@ import { getSession } from 'next-auth/react'
 import Stripe from 'stripe'
 import { buffer } from 'micro'
 
-//not just for vercel
 export const config = {
   api: {
     bodyParser: false
@@ -24,36 +23,24 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log('1')
   if (req.method === 'POST') {
-    console.log('2')
     const sig = req.headers['stripe-signature']!
     const buf = await buffer(req)
     const body: string = buf.toString()
 
-    console.log('secret ', webhookSecret)
-    console.log('sig ', sig)
-    console.log('request body', body)
-
-    console.log('stripe key', process.env.STRIPE_KEY!)
-    // console.log('stripe ', stripe)
     let event: any
     try {
-      console.log('3')
       event = stripe.webhooks.constructEvent(body, sig, webhookSecret)
-      console.log('4')
-
       switch (event.type) {
         //! in production you must specify which events to listen to in your webhook on stripe.com
         case 'customer..subscription.updated':
           {
-            console.log('created')
             const product = await stripe.products.retrieve(
               event.data.object.plan.product
             )
-
             const id = event.data.object.metadata.userId
             const tier = product.name
+
             await prisma.user.update({
               where: {
                 id
@@ -66,10 +53,7 @@ export default async function handler(
           break
         case 'customer.subscription.updated':
           {
-            console.log('updated')
-
             const id = event.data.object.metadata.userId
-            console.log('user id: ', id)
             //! not importing from util/stripe
             const product = await stripe.products.retrieve(
               event.data.object.plan.product
@@ -87,12 +71,9 @@ export default async function handler(
           break
 
         case 'payment_intent.failed':
-          // console.log('payment failed')
           break
         default: {
-          const newSession = await getSession()
-          console.log(newSession)
-          // console.log(`Unhandled event type: ${event.type}`)
+          return
         }
       }
     } catch (err) {
@@ -103,11 +84,3 @@ export default async function handler(
 
   res.json({ received: true })
 }
-
-// async function buffer(readable) {
-//   const chunks = []
-//   for await (const chunk of readable) {
-//     chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
-//   }
-//   return Buffer.concat(chunks)
-// }
